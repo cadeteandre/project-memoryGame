b9a5d222c61ec976d50a2ba0f4a94695be6ea51e
Lamia Rizk <lamiarizk@gmx.de>
2024-11-21 13:20:45 +0100

    start shuffle function

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,17 @@ const pairsGuessed = document.querySelector('#pairsGuessed') as HTMLParagraphEle
 const cards = document.querySelectorAll('.card') as NodeListOf<HTMLDivElement>;
 
 const emojiArr: string[] = ['🥳', '😍', '😃', '😡', '🤢', '🥶', '😎', '😴', '🤕', '🤠', '🤣', '😱'];
+// const cardsForComparision: string[] = [...emojiArr, ...emojiArr];
+
+// const shuffleAllCards = (array: string[]): string[] => 
+
+function shuffleAllCards(array: string[]): string[] {
+    for (let i = 0; i < array.length; i++) {
+        const randomNum = Math.floor(Math.random() * array.length)
+    }
+}
+
+
 
 for(let i: number = 0; i < cards.length; i++) {
     if(i >= emojiArr.length) {
@@ -19,6 +30,3 @@ cards.forEach((card: HTMLDivElement) => {
     card.addEventListener('click', () => {
     })
 })
-
-
- 

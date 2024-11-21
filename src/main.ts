34b5be51706f6393e2cd5e57b7b10190f84222cd
Andre Cadete <andrecadete70@gmail.com>
2024-11-21 11:41:20 +0100

    emojis create

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import './style.css'
+
+//* --------------- Selection HTML elements ---------------
+const pairsClicked = document.querySelector('#pairsClicked') as HTMLParagraphElement;
+const pairsGuessed = document.querySelector('#pairsGuessed') as HTMLParagraphElement;
+const cards = document.querySelectorAll('.card') as NodeListOf<HTMLDivElement>;
+
+const emojiArr: string[] = ['🥳', '😍', '😃', '😡', '🤢', '🥶', '😎', '😴', '🤕', '🤠', '🤣', '😱'];
+
+for(let i: number = 0; i < cards.length; i++) {
+    if(i >= emojiArr.length) {
+        cards[i].innerHTML = `<div class="emoji">${emojiArr[i - emojiArr.length]}</div>`;
+    } else {
+        cards[i].innerHTML = `<div class="emoji">${emojiArr[i]}</div>`;
+    }
+}
+
+cards.forEach((card: HTMLDivElement) => {
+    card.addEventListener('click', () => {
+    })
+})
